b5ca46a5cd82e3edf34428235b47e5741fa62088
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-03-13 14:15:05 -0300

    Login e cadastro implementados

---
+++ scripts/cadastro.js
@@ -0,0 +1,12 @@
+const formCadastro = document.getElementById('form-Cadastro')
+const nomeCadastro = document.getElementById('nomeCadastro')
+const emailCadastro = document.getElementById('emailCadastro')
+const senhaCadastro = document.getElementById('senhaCadastro')
+
+formCadastro.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    sessionStorage.setItem("nome", nomeCadastro.value)
+    sessionStorage.setItem("email", emailCadastro.value)
+    sessionStorage.setItem("senha", senhaCadastro.value)
+    window.location.href="http://localhost:5500/index.html"
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
 const formLogin = document.getElementById('form-login');
 const emailLogin = document.getElementById('email')
 const senhaLogin = document.getElementById('senha')
-localStorage.setItem("email", "");
-localStorage.setItem("senha", "");
-let user ={
-    email:localStorage.getItem("email"),
-    senha:localStorage.getItem("senha")
-}
+
 formLogin.addEventListener('submit', (e)=>{
     e.preventDefault()
-    localStorage.setItem("email", emailLogin.value);
-    localStorage.setItem("senha", senhaLogin.value);
-    console.log(localStorage.getItem("email"))
+    if(emailLogin.value == sessionStorage.getItem("email")&&senhaLogin.value==sessionStorage.getItem("senha")){
+        console.log("ok")
+    }
+    else{
+        console.log("errado")
+    }
+
 })
+
 
