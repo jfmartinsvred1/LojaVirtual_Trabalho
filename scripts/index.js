d7845d5eb4942a2f60c963fd02dfe33c3dc2dd3f
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-03-13 19:25:49 -0300

    fsdfd

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ const formLogin = document.getElementById('form-login');
 const emailLogin = document.getElementById('email')
 const senhaLogin = document.getElementById('senha')
 
+console.log(sessionStorage.getItem("email"))
+console.log(sessionStorage.getItem("senha"))
+
 formLogin.addEventListener('submit', (e)=>{
     e.preventDefault()
     if(
@@ -9,7 +12,7 @@ formLogin.addEventListener('submit', (e)=>{
         &&senhaLogin.value==sessionStorage.getItem("senha")
     )
     {
-        window.location.href="http://localhost:5500/produtos.html"
+        window.location.href="../produtos.html"
     }
     else{
         /*caso login de errado*/
