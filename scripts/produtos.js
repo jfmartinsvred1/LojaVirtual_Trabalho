d3c1b2b272566bc8d65f6992a7ffcc7016c960ec
jfmartinsvred1 <jfmartinsvred@gmail.com>
2024-03-18 14:36:13 -0300

    Colocando opcao com desconto

--- scripts/produtos.js
+++ scripts/produtos.js
@@ -10,37 +10,43 @@ const produtos=
         id:1,
         img:"../images/productsImages/Product1/Tênis-Nike-Air-Max-90-Masculino-1.jpg",
         nome:"Tênis Nike Air Max 90 Masculino",
-        preco:799.99
+        preco:799.99,
+        desconto:0.0
     },
     {
         id:2,
         img:"../images/productsImages/Product2/Tênis-Nike-Air-Force-1-Flyease-1.jpg",
         nome:"Tênis Nike Air Force 1 Flyease",
-        preco:799.99
+        preco:799.99,
+        desconto:0.0
     },
     {
         id:3,
         img:"../images/productsImages/Product3/SB-Dunk-Low-Pro-1.jpg",
         nome:"SB Dunk Low Pro",
-        preco:899.99
+        preco:899.99,
+        desconto:30.0
     },
     {
         id:4,
         img:"../images/productsImages/Product4/Air-Jordan-1-Retro-Low-OG-1.jpg",
         nome:"Air Jordan 1 Retro Low OG",
-        preco: 1099.00
+        preco: 1099.00,
+        desconto:0.0
     },
     {
         id:5,
         img:"../images/productsImages/Product5/Tênis-Nike-SB-Force-58-Unissex-6.jpg",
         nome:"Tênis Nike SB Force 58 Unissex",
-        preco:479.99
+        preco:479.99,
+        desconto:0.0
     },
     {
         id:6,
         img:"../images/productsImages/Product6/Air-Force-1-x-AMBUSH-1.jpg",
         nome:"Air Force 1 x AMBUSH",
-        preco:1499.90
+        preco:1499.90,
+        desconto:0.0
     }
 ]
 
@@ -66,12 +72,31 @@ const alunos = [
 function retornaHtmlProdutos(){
     let html=``
     for(let i=0;i<=produtos.length-1;i++){
-        html += `
+        if(produtos[i].desconto!=0){
+            let produtoComDesconto = produtos[i].preco * (1-(produtos[i].desconto/100))
+            html += `
+        <div class="sneaker">
+            <img src=${produtos[i].img} alt=${produtos[i].nome}>
+            <h3>${produtos[i].nome}</h3>
+            <h4>De 
+                <s>
+                    R$ ${produtos[i].preco} 
+                </s>
+                por apenas R$ 
+                <b id="promo">
+                    ${produtoComDesconto.toFixed(2)}
+                </b>
+            </h4>
+        </div>`
+        }
+        else{
+            html += `
         <div class="sneaker">
             <img src=${produtos[i].img} alt=${produtos[i].nome}>
             <h3>${produtos[i].nome}</h3>
             <h4>R$ ${produtos[i].preco}</h4>
         </div>`
+        }
     }
     divProducts.innerHTML=html
 }
